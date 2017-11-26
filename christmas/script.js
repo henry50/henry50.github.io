a4be0277b5277f7d0fc55183a6dd766d36867165
H. Lunn <henry50@users.noreply.github.com>
2017-11-26 11:04:49 +0000

    Added christmas homepage

---
+++ christmas/script.js
@@ -0,0 +1,44 @@
+//Countdown
+var year = new Date().getFullYear();
+var datestr = "12/25/"+year;
+var end = new Date(datestr);
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('dtc').innerHTML = "Merry Christmas!";
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById("dtc").innerHTML = days+1;
+
+}
+
+timer = setInterval(showRemaining, 1000);
+//Search
+function search(){
+    window.alert("called")
+    var inp = document.getElementById("search");
+    var query = inp.value;
+    var glink = "https://goo.gl/search/" + query;
+    inp.value = "";
+    document.location.href = glink;
+}
+document.body.onkeydown=function(cc){
+    if(cc.keyCode==13){
+        search()
+    }
+}
